da7294abe979befe3ad584a7db622cbf87346627
Gabriel Laroche <144166325+gablaroche29@users.noreply.github.com>
2026-03-05 00:13:01 -0500

    feat: add websockets listener

---
+++ network/server_listener.gd
@@ -0,0 +1,26 @@
+extends Node
+
+var websocket := WebSocketPeer.new()
+var url := "ws://localhost:3000"
+
+func _ready():
+	print("Connecting to WebSocket server:", url)
+	var err = websocket.connect_to_url(url)
+	
+	if err != OK:
+		print("WebSocket connection failed:", err)
+	else:
+		print("WebSocket connection started")
+
+func _process(_delta):
+	websocket.poll()
+
+	var state = websocket.get_ready_state()
+
+	if state == WebSocketPeer.STATE_OPEN:
+		while websocket.get_available_packet_count():
+			var message = websocket.get_packet().get_string_from_utf8()
+			print("[WS EVENT]: ", message)
+
+	elif state == WebSocketPeer.STATE_CLOSED:
+		print("WebSocket closed")
