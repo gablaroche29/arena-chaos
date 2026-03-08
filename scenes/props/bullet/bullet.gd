f57b905efc833394f02d26e08c32840da292a53e
Gabriel Laroche <144166325+gablaroche29@users.noreply.github.com>
2026-03-07 22:19:07 -0500

    fix: fix bugs with enemy collisions and bullet

--- scenes/props/bullet/bullet.gd
+++ scenes/props/bullet/bullet.gd
@@ -3,16 +3,23 @@ extends Node2D
 @export var speed: float = 800.0
 @export var damage: int = 2
 
+var has_hit := false
+
 func _physics_process(delta: float) -> void:
 	global_position += Vector2.RIGHT.rotated(rotation) * speed * delta
 
 func _on_area_2d_body_entered(body: Node) -> void:
+	if has_hit:
+		return
+
 	if body.is_in_group("enemy"):
+		has_hit = true
+		
 		var health = body.get_node_or_null("Health")
 		if health and health is Health:
 			health.damage(damage)
 
-	queue_free()
+		queue_free()
 
 func _on_distance_timeout_timeout() -> void:
 	queue_free()
